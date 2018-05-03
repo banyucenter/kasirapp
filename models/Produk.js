5ab862924f2de2442f8257284f45359c833ddebb
ipung purwono <ipungofficial@gmail.com>
2018-05-03 12:05:57 +0700

    folder models

---
+++ models/Produk.js
@@ -0,0 +1,10 @@
+
+var mongoose = require('mongoose');
+
+var ProdukSchema = new mongoose.Schema({
+	nama: String,
+	kategori: String,
+	biaya: Number
+});
+
+module.exports = mongoose.model('Produk', ProdukSchema);
